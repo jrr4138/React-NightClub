d7e5589511833ab68c03b8d76c83c3adcdeb2db5
jrr4138 <69919312+jrr4138@users.noreply.github.com>
2021-11-02 21:43:47 -0400

    Made boxes with border-radius and correct sizes

--- javascript/index-1.js
+++ javascript/index-1.js
@@ -1,4 +1,55 @@
-function menu(id) {
+function club1(id) {
+    x = document.getElementById(id);
+    if(x == "club_arcane") {
+        x.value = "Name: Club Arcane\nLocation: Tonanto\nMusic: Pop";
+    } else if(x == "2") {
+        x.value = "Name: Club Underground\nLocation: Los Angeles\nMusic: Rock";
+    } else if(x == "3") {
+        x.value = "Name: Club Soda\nLocation: Montreal\nMusic: Jazz";
+    } else if(x == "4") {
+        x.value = "Name: Studio 52\nLocation: New York City\nMusic: Pop";
+    } else if(x == "5") {
+        x.value = "Name: Circo's\nLocation: Woodbridge\nMusic: Rock";
+    } else if(x == "6") {
+        x.value = "Name: Paradisio Garage\nLocation: New York City\nMusic: Rock";
+    }
+}
+
+function club2(id) {
+    x = document.getElementById(id);
+    if(x == "club_arcane") {
+        x.value = "Name: Club Arcane\nLocation: Tonanto\nMusic: Pop";
+    } else if(x == "2") {
+        x.value = "Name: Club Underground\nLocation: Los Angeles\nMusic: Rock";
+    } else if(x == "3") {
+        x.value = "Name: Club Soda\nLocation: Montreal\nMusic: Jazz";
+    } else if(x == "4") {
+        x.value = "Name: Studio 52\nLocation: New York City\nMusic: Pop";
+    } else if(x == "5") {
+        x.value = "Name: Circo's\nLocation: Woodbridge\nMusic: Rock";
+    } else if(x == "6") {
+        x.value = "Name: Paradisio Garage\nLocation: New York City\nMusic: Rock";
+    }
+}
+
+function club3(id) {
+    x = document.getElementById(id);
+    if(x == "club_arcane") {
+        x.value = "Name: Club Arcane\nLocation: Tonanto\nMusic: Pop";
+    } else if(x == "2") {
+        x.value = "Name: Club Underground\nLocation: Los Angeles\nMusic: Rock";
+    } else if(x == "3") {
+        x.value = "Name: Club Soda\nLocation: Montreal\nMusic: Jazz";
+    } else if(x == "4") {
+        x.value = "Name: Studio 52\nLocation: New York City\nMusic: Pop";
+    } else if(x == "5") {
+        x.value = "Name: Circo's\nLocation: Woodbridge\nMusic: Rock";
+    } else if(x == "6") {
+        x.value = "Name: Paradisio Garage\nLocation: New York City\nMusic: Rock";
+    }
+}
+
+function club4(id) {
     x = document.getElementById(id);
     if(x == "club_arcane") {
         x.value = "Name: Club Arcane\nLocation: Tonanto\nMusic: Pop";
