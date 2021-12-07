a58dbf0502fcec2dc3fd40b3dc1525cabd4d8995
jrr4138 <69919312+jrr4138@users.noreply.github.com>
2021-12-07 14:20:55 -0500

    Figuring stuff out

--- jrr4138-react/src/Clubs.js
+++ jrr4138-react/src/Clubs.js
@@ -5,11 +5,22 @@ class Clubs extends React.Component{
 
     constructor(props) {
         super(props);
-        
-        this.state = {}
+        let values = {name: props.name, location: props.location, music: props.music, yellow: props.yellow, red: props.red}
+        this.state = {count: 0, clubDetails: values}
     };
 
-    
+    incrementClub=()=>{
+
+    }
+
+    decrementClub=()=>{
+
+    }
+
+    /* Might need some params here, not sure how it will work yet */
+    createClub=()=>{
+
+    }
 
     render(){
         return(
@@ -17,64 +28,64 @@ class Clubs extends React.Component{
                 <Row>
                     <Col sm="3">
                         <Card body color="primary" inverse>
-                            <CardTitle tag="h5">
+                            <CardTitle tag="h3">
                                 Club Arcane
                             </CardTitle>
-                            <CardText tag="h3">
-                                Our club is cool!
+                            <CardText tag="h5">
+                                Music: {}
                                 <br></br>
                                 <br></br>
                                 0
                             </CardText>
                             <br></br>
-                            <Button color="success">increment</Button>
+                            <Button color="success" onClick={this.incrementClub}>increment</Button>
                             <br></br>
-                            <Button color="danger">decrement</Button>
+                            <Button color="danger" onClick={this.decrementClub}>decrement</Button>
                         </Card>
                     </Col>
 
                     <Col sm="3">
                         <Card body color="warning" inverse>
-                            <CardTitle tag="h5">
-                                Club 2
+                            <CardTitle tag="h3">
+                                Club Underground
                             </CardTitle>
-                            <CardText>
+                            <CardText tag="h5">
                                 Our club is rad!
                                 <br></br>
                                 <br></br>
                                 0
                             </CardText>
                             <br></br>
-                            <Button color="success">increment</Button>
+                            <Button color="success" onClick={this.incrementClub}>increment</Button>
                             <br></br>
-                            <Button color="danger">decrement</Button>
+                            <Button color="danger" onClick={this.decrementClub}>decrement</Button>
                         </Card>
                     </Col>
 
                     <Col sm="3">
                         <Card body color="info" inverse>
-                            <CardTitle tag="h5">
-                                Club 3
+                            <CardTitle tag="h3">
+                                Club Soda
                             </CardTitle>
-                            <CardText>
+                            <CardText tag="h5">
                                 Our club is awesome!
                                 <br></br>
                                 <br></br>
                                 0
                             </CardText>
                             <br></br>
-                            <Button color="success">increment</Button>
+                            <Button color="success" onClick={this.incrementClub}>increment</Button>
                             <br></br>
-                            <Button color="danger">decrement</Button>
+                            <Button color="danger" onClick={this.decrementClub}>decrement</Button>
                         </Card>
                     </Col>
                     
                     <Col sm="3">
                         <Card body color="secondary" inverse>
-                            <CardTitle tag="h5">
-                                Club 4
+                            <CardTitle tag="h3">
+                                Studio 52
                             </CardTitle>
-                            <CardText>
+                            <CardText tag="h5">
                                 Our club is fantastic!
                                 <br></br>
                                 <br></br>
@@ -84,39 +95,9 @@ class Clubs extends React.Component{
                             <Button color="success" onClick={this.incrementClub}>increment</Button>
                             <br></br>
                             <Button color="danger" onClick={this.decrementClub}>decrement</Button>
-                    </Card>
-                </Col>
-            </Row><br></br>
-            
-            <div id={this.state.ArcColor}>Club Arcane
-                    <br></br>
-                    <p id="ClubArcaneMessage">{this.ClubArcaneMessage}</p>
-                </div><div id={this.state.UndColor}>Club Underground
-                    <br></br>
-                    <p id="ClubUndergroundMessage">{this.ClubUndergroundMessage}</p>
-                </div><div id={this.state.SodColor}>Club Soda
-                    <br></br>
-                    <p id="ClubSodaMessage">{this.ClubSodaMessage}</p>
-                </div><div id={this.state.StuColor}>Studio 52
-                    <br></br>
-                    <p id="StudioMessage">{this.ClubStudioMessage}</p>
-                </div><br></br><p id="ClubArcaneCount">{this.state.countArcane}</p><p id="ClubUndergroundCount">{this.state.countUnderground}</p><p id="ClubSodaCount">{this.state.countSoda}</p><p id="StudioCount">{this.state.countStudio}</p><br></br><h2>
-                    <input type="radio" id="arcane" name="club" value="Club Arcane" onChange={(i) => this.clubSelect(i)}></input>
-                    <label for="Arcane">Club Arcane</label>
-                    <br></br>
-                    <input type="radio" id="underground" name="club" value="Club Underground" onChange={(i) => this.clubSelect(i)}></input>
-                    <label for="Underground">Club Underground</label>
-                    <br></br>
-                    <input type="radio" id="soda" name="club" value="Club Soda" onChange={(i) => this.clubSelect(i)}></input>
-                    <label for="Soda">Club Soda</label>
-                    <br></br>
-                    <input type="radio" id="studio" name="club" value="Studio 52" onChange={(i) => this.clubSelect(i)}></input>
-                    <label for="Studio">Studio 52</label>
-                    <br></br>
-                    <br></br>
-                    <input type="button" name="+" value="+" onClick={(i) => this.buttonClick(i)}></input>
-                    <input type="button" name="-" value="-" onClick={(i) => this.buttonClick(i)}></input>
-                </h2>
+                        </Card>
+                    </Col>
+                </Row><br></br>
             </div>
         )
     }
